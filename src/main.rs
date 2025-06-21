5b7ebaa3bb029dd99d68845acd77873d334f6c17
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-06-22 01:12:59 +0400

    feat: handle set requests

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
+use std::collections::HashMap;
 use std::net::{TcpListener, TcpStream};
-use std::io::{Read};
+use std::io::{Read, Write};
 
-fn client_handler(mut stream: TcpStream) {
+fn client_handler(mut stream: TcpStream, data: &mut HashMap<String, String>) {
     let mut request: String = String::new();
 
     stream.read_to_string(&mut request).unwrap().to_string();
 
-    request = request.lines().next().unwrap().to_string();
-    let request_type: String = request.split(' ').nth(1).unwrap()[1..4].to_string();
+    request = request.lines().next().unwrap().split(' ').nth(1).unwrap().to_string();
+    let request_type: String = request[1..4].to_string();
 
     match request_type.as_str() {
         "set" => {
-            todo!()
+            let rest = request[5..].to_string();
+            let parts: Vec<&str> = rest.split('=').collect();
+            let key = parts[0].to_string();
+            let value = parts[1].to_string();
+            println!("{} - {}", &key, &value);
+            data.insert(key, value);
         }
 
         "get" => {
@@ -23,19 +29,21 @@ fn client_handler(mut stream: TcpStream) {
         }
     }
 
-    println!("{request_type}");
+    // println!("{request_type}");
 }
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4000").unwrap();
+    let mut data: HashMap<String, String> = HashMap::new();
 
     println!("Listening on localhost... 🦀 👀");
 
+    // TODO: Connections persist unless forced to terminate
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
-                client_handler(stream);
+                client_handler(stream, &mut data);
                 println!("Connection closed");
             }
 
