0b88d8a6ec06e129fb781058471120edd722e5bf
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-06-21 18:14:13 +0400

    feat(server): add basic TCP server with client handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::net::{TcpListener, TcpStream};
+use std::io::{Read};
+
+fn client_handler(mut stream: TcpStream) {
+    let mut input: String = String::new();
+
+    stream.read_to_string(&mut input).unwrap().to_string();
+    println!("{input}");
+}
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4000").unwrap();
-    println!("listening on localhost... 🦀 👀");
+
+    println!("Listening on localhost... 🦀 👀");
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
+                client_handler(stream);
+                println!("Connection closed");
             }
 
             Err(e) => {
