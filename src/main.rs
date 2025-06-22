8796a186f38e8020b5df243cf5580bce6fc8b591
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-06-22 18:56:09 +0400

    refactor: improve error handling

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ fn parse_request(request: &String) -> (String, String) {
     (request_type, rest)
 }
 
-fn client_handler(mut stream: TcpStream, data: &mut HashMap<String, String>) {
+fn client_handler(mut stream: TcpStream, data: &mut HashMap<String, String>) -> Result<String, &str> {
     let mut buffer = [0; 1024];
 
-    let request_size = stream.read(&mut buffer).unwrap();
+    let request_size = stream.read(&mut buffer).map_err(|_| "Error saving to buffer")?;
     let mut request = String::from_utf8_lossy(&buffer[..request_size]).to_string();
 
-    request = request.lines().next().unwrap().split(' ').nth(1).unwrap().to_string();
+    let line = request.lines().next().ok_or("Request line missing")?.to_string();
+    request = line.split(' ').nth(1).ok_or("Request not found")?.to_string();
     let (request_type, rest) = parse_request(&request);
 
     match request_type.as_str() {
@@ -23,23 +24,35 @@ fn client_handler(mut stream: TcpStream, data: &mut HashMap<String, String>) {
             let parts: Vec<&str> = rest.split('=').collect();
             let key = parts[0].to_string();
             let value = parts[1].to_string();
-            println!("{} - {}", &key, &value);
-            data.insert(key, value);
+
+            data.insert(key.clone(), value.clone());
+            let response = format!("HTTP/1.1 200 OK\r\nConnection: close\r\n\r\nOK");
+            let _ = stream.write_all(response.as_bytes());
+
+            Ok(format!("set {}={}", &key, &value))
         }
 
         "get" => {
             let parts: Vec<&str> = rest.split('=').collect();
             let key = parts[1].to_string();
-            let value = data.get(&key).unwrap();    // TODO: Handle non existent pairs
+            let value = match data.get(&key) {
+                Some(v) => {
+                    v
+                }
 
-            println!("{}", &value);
+                None => {
+                    "ERROR: Invalid key"
+                }
+            };
 
             let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\nConnection: close\r\n\r\n{}", value.len(), value);
-            let _ = stream.write_all(response.as_bytes()).unwrap();
+            let _ = stream.write_all(response.as_bytes());
+
+            Ok(value.to_string())
         }
 
         _ => {
-            println!("Invalid request")
+            Err("Invalid request")
         }
     }
 
@@ -50,14 +63,22 @@ fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4000").unwrap();
     let mut data: HashMap<String, String> = HashMap::new();
 
-    println!("Listening on localhost... 🦀 👀");
+    println!("Listening on localhost... 🦀 👀\n");
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
-                client_handler(stream, &mut data);
-                println!("Connection closed");
+                match client_handler(stream, &mut data) {
+                    Ok(result) => {
+                        println!("{}", result.to_string());
+                    }
+
+                    Err(e) => {
+                        println!("{}", e.to_string());
+                    }
+                }
+                println!("Connection closed\n");
             }
 
             Err(e) => {
